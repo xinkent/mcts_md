06df7aa504fc2c15dc2279adbd514639050cd223
xinkent <xinkent1994@gmail.com>
2018-06-02 17:13:24 +0900

    update

--- pacs_md.py
+++ pacs_md.py
@@ -56,9 +56,9 @@ def initialize_target():
 
 def first_cycle(n):
     print(n)
-    os.system('gmx_mpi grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_%d.tpr -maxwarn 5' % n)
+    os.system('gmx_mpi grompp -f md.mdp -c 0.gro -t 0.cpt -p topol.top -o md_0_%d.tpr -maxwarn 5' % n)
     os.system('gmx_mpi mdrun -deffnm md_0_%d' % n)
-    os.system("echo 4 4 | gmx_mpi rms -s 150l_npt.gro -f md_0_%d.trr  -o rmsd_0_%d.xvg -tu ns" % (n, n))
+    os.system("echo 4 4 | gmx_mpi rms -s target_npt.gro -f md_0_%d.trr  -o rmsd_0_%d.xvg -tu ns" % (n, n))
 
 def md_cycle(args):
     step = args[0]
@@ -71,7 +71,7 @@ def md_cycle(args):
     os.system('echo 0 | gmx_mpi trjconv -s %s.tpr -f %s.trr -o %s.gro -e %d' % (input, input, temp, md_ind[1]))
     os.system('gmx_mpi grompp -f md.mdp -t %s.trr -o %s.tpr -c %s.gro -maxwarn 5' %(temp, md, temp))
     os.system('gmx_mpi mdrun -deffnm %s' % md)
-    os.system("echo 4 4 | gmx_mpi rms -s 150l_npt.gro -f %s.trr  -o rmsd_%d_%d.xvg -tu ns" % (md, step, n))
+    os.system("echo 4 4 | gmx_mpi rms -s target_npt.gro -f %s.trr  -o rmsd_%d_%d.xvg -tu ns" % (md, step, n))
 
 
 
@@ -93,9 +93,10 @@ dt = 1 # ps for each step
 nsteps = 101 # step for each md including inital state
 n_para = 5
 MAX_CYCLE = 100
+pass_flag = False
 
-cycle_step = 40
-log = np.zeros((MAX_CYCLE - cycle_step + 1, n_para))
+cycle_step = 0
+log = np.zeros((MAX_CYCLE - cycle_step, n_para))
 # initialize()
 # initialize_target()
 
@@ -108,8 +109,8 @@ log = np.zeros((MAX_CYCLE - cycle_step + 1, n_para))
 
 
 # 途中から再開する場合
-pass_flag = True
-cycle_step -= 1
+# pass_flag = True
+# cycle_step -= 1
 log_i = 0
 
 while cycle_step < MAX_CYCLE:
@@ -132,5 +133,5 @@ while cycle_step < MAX_CYCLE:
     log_i += 1
 
 
-np.savetxt('log_39_100.csv', log, delimiter=',')
+np.savetxt('log_0_100.csv', log, delimiter=',')
 
