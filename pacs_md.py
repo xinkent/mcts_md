57104cb3a58285763d4e0b1cf31a2463fd396330
xinkent <xinkent1994@gmail.com>
2018-06-07 12:10:45 +0900

    update

--- pacs_md.py
+++ pacs_md.py
@@ -22,8 +22,6 @@ def md_cycle(args):
     os.system('gmx_mpi mdrun -deffnm %s' % md)
     os.system("echo 4 4 | gmx_mpi rms -s target_npt.gro -f %s.trr  -o rmsd_%d_%d.xvg -tu ns" % (md, step, n))
 
-
-
 def read_rmsd(step, n):
     f = open('rmsd_%d_%d.xvg' % (step, n))
     rmsd = []
@@ -37,11 +35,36 @@ def read_rmsd(step, n):
     f.close()
     return rmsd
 
-def pacs_md()
+def modify_rmsd(ip, op):
+    f = open(ip)
+    o = open(op, 'w')
+    while True:
+        l = f.readline()
+        if l[0] != '#' and l[0] != '@':
+            break
+        else:
+            o.write(l)
+
+    cum_time = 0.0
+    rmsd = l.split()[1]
+    o.write(str(cum_time) + "    " + str(rmsd) + "\n")
+    l = f.readline()
+    cum_time += 1.0
+
+    while l != '':
+        time = float(l.split()[0])
+        rmsd = l.split()[1]
+        if time != 0.0:
+            o.write(str(cum_time) + "    " + str(rmsd) + "\n")
+            cum_time += 1.0
+        l = f.readline()
+    f.close()
+    o.close()
+
+
+def pacs_md(MAX_CYCLE = 100, n_para = 5)
     dt = 1 # ps for each step
     nsteps = 101 # number of steps for each md including inital state
-    n_para = 5 # pacs_mdの並列数
-    MAX_CYCLE = 100 
     pass_flag = False
     cycle_step = 0
     log = np.zeros((MAX_CYCLE - cycle_step, n_para))  # trajectory間の接続を記録
@@ -71,3 +94,44 @@ def pacs_md()
 
     np.savetxt('log_0_100.csv', log, delimiter=',')
 
+def cocat_traj():
+    log = np.loadtxt("log_0_100.csv", delimiter = ",")
+    back_list = []
+    step = log.shape[0] - 1
+    log_index = 0
+
+    for i in range(step,0, -1):
+        log_index = int(log[i, log_index])
+        back_list.insert(0, log_index)
+    # print(back_list)
+    trajs = []
+    for i, j in enumerate(back_list):
+        traj = "res_%d_%d"%(i,j)
+        print(traj)
+        trajs.append(traj)
+
+    traj_str = ""
+    for traj in trajs:
+        traj_str += (traj + " ")
+
+    # print(traj_str)
+    os.system( "gmx_mpi trjcat -f " + traj_str + " -o merged_pacs.trr ")
+    os.system("gmx_mpi rms -s 150l_npt.gro -f merged_pacs.trr -tu ns -o rmsd_merged_tmp.xvg")
+    modify_rmsd('rmsd_pacs_tmp.xvg', 'rmsd_pacs.xvg')
+    os.remove('rmsd_pacs_tmp.trr')
+
+# 各ステップでのrmsdを記録
+def write_log(m):
+    o = open('min_rmsd.txt','w')
+    for i in range(m):
+        rmsd = read_rmsd(i,0)
+        o.write(rmsd[0] + '\n')
+    o.close()
+
+
+def main():
+    M = 100 # サイクル数
+    k = 5 # 並列数
+    pacs_md(M, k)
+    cancat_traj()
+    write_log(m)
