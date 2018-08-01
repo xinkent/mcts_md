c7d4efafb45fbd5886deca545aabf168f09b2a6e
xinkent <xinkent1994@gmail.com>
2018-08-01 20:32:53 +0900

    bug fix

--- util.py
+++ util.py
@@ -47,14 +47,17 @@ def make_tree_pacs(log_file):
     G = Graph(format='svg')
     G.attr('node', shape='circle')
     G.graph_attr.update(size="320")
-    state = 0
     G.node('0-0', '0-0')
-    state += 1
+    for i in range(5):
+        state = '1-' + str(i)
+        G.node(state, state)
+        G.edge('0-0', state)
+
     for i in range(len(log)):
         log_i = log[i]
         for j, l in enumerate(log_i):
-            state = str(i + 1) + '-' + str(j)
-            pstate = str(i) + '-' + str(int(l))
+            state = str(i + 2) + '-' + str(j)
+            pstate = str(i + 1) + '-' + str(int(l))
             G.node(state, state)
             G.edge(pstate, state)
     G.render('tree_pacs')
