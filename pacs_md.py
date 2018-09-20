b00691fdb55d669977de05bdbc610bd1e091c777
xinkent <xinkent1994@gmail.com>
2018-09-20 17:44:40 +0900

    update

--- pacs_md.py
+++ pacs_md.py
@@ -3,6 +3,8 @@ import os, glob
 from multiprocessing import Pool
 from util import *
 
+MIN_RMSD = 0.1
+
 def first_cycle(n):
     # print(n)
     os.system('gmx grompp -f md.mdp -c 0.gro -t 0.cpt -p topol.top -o md_0_%d.tpr -maxwarn 5' % n)
@@ -18,8 +20,10 @@ def md_cycle(args):
     temp   = 'res_%d_%d' % (step-1, n)
     md     = 'md_%d_%d' % (step, n)
     rmsd   = 'rmsd_%d_%d' % (step, n)
+    # 全ステップで計算したトラジェクトリを指定の部分まで切り取る
     os.system('echo 0 | gmx trjconv -s %s.tpr -f %s.trr -o %s.trr -e %d' % (input, input, temp, md_ind[1]))
     os.system('echo 0 | gmx trjconv -s %s.tpr -f %s.trr -o %s.gro -e %d' % (input, input, temp, md_ind[1]))
+    # 切り取った箇所から新たにMD
     if not last:
         os.system('gmx grompp -f md.mdp -t %s.trr -o %s.tpr -c %s.gro -maxwarn 5' %(temp, md, temp))
         os.system('gmx mdrun -deffnm %s' % md)
@@ -28,48 +32,61 @@ def md_cycle(args):
 def pacs_md(MAX_CYCLE = 100, n_para = 5):
     dt = 1 # ps for each step
     nsteps = 101 # number of steps for each md including inital state
-    pass_flag = False
+    pass_flag = False 
     cycle_step = 0
-    log = np.zeros((MAX_CYCLE - cycle_step, n_para))  # trajectory間の接続を記録
+    edge_log = np.zeros((MAX_CYCLE - cycle_step, n_para))  # Treeの遷移関係のlog
     log_i = 0
+    o = open('log_pacs.txt','w')
+    o.close()
     # 途中(100回目)から再開する場合
     # pass_flag = True
     # cycle_step = 100
 
-    while cycle_step < MAX_CYCLE:
+    while cycle_step < MAX_CYCLE or min_rmsd < MIN_RMSD:
         if not pass_flag:
             if cycle_step == 0:
                 for i in range(n_para):
                     first_cycle(i)
             else:
                 for i in range(n_para):
-                    md_cycle([cycle_step, i, min_rmsd[i],False])
+                    md_cycle([cycle_step, i, min_rmsd_idx[i],False])
         else:
             pass_flag = False
+        # 最小RMSDを記録
         rmsd_list = np.zeros(n_para * nsteps)
         for i in range(n_para):
             rmsd_list[i * nsteps : (i+1) * nsteps] = read_rmsd('rmsd_%d_%d.xvg'%(cycle_step, i))
-        min_rmsd = rmsd_list.argsort()[:n_para]
-        min_rmsd = [(r // nsteps, r % nsteps) for r in min_rmsd]
-        log[log_i,:] = [r[0] for r in min_rmsd]
+        min_rmsd = min(rmsd_list)
+       
+        if cycle_step = 0:
+            first_rmsd = rmsd_list[0]
+            write_log(first_rmsd)
+        write_log(min_rmsd)
+        # Treeの遷移関係を記録
+        min_rmsd_idx = rmsd_list.argsort()[:n_para]
+        min_rmsd_idx = [(r // nsteps, r % nsteps) for r in min_rmsd_idx]
+        edge_log[log_i,:] = [r[0] for r in min_rmsd_idx]
+        
         cycle_step += 1
         log_i += 1
-
+    
+    # logをファイルに保存(concat_traj)に渡す
     for i in range(n_para):
-        md_cycle([cycle_step, i, min_rmsd[i],True])
-    np.savetxt('log_0_100.csv', log, delimiter=',')
+        md_cycle([cycle_step, i, min_rmsd_idx[i],True])
+    np.savetxt('edge_log.csv', edge_log, delimiter=',')
 
 # log.csvを元にshort MDのトラジェクリを繋げる
 def concat_traj():
-    log = np.loadtxt("log_0_100.csv", delimiter = ",")
+    log = np.loadtxt("edge_log.csv", delimiter = ",")
     back_list = [0]
     step = log.shape[0] - 1
     log_index = 0
-
+    # 最もRMSDが小さくなったものについて、backtrackする
     for i in range(step,0, -1):
         log_index = int(log[i, log_index])
         back_list.insert(0, log_index)
     # print(back_list)
+    # トラジェクトリを結合
     trajs = []
     for i, j in enumerate(back_list):
         traj = "res_%d_%d"%(i,j)
@@ -79,18 +96,15 @@ def concat_traj():
     for traj in trajs:
         traj_str += (traj + " ")
     # print(traj_str)
-
     os.system( "gmx trjcat -f " + traj_str + " -o merged_pacs.trr -cat")
     os.system("echo 4 4| gmx rms -s target_processed.gro -f merged_pacs.trr -tu ns -o rmsd_pacs_tmp.xvg")
-    modify_rmsd('rmsd_pacs_tmp.xvg', 'rmsd_pacs.xvg')
+    modify_rmsd('rmsd_pacs_tmp.xvg', 'rmsd_pacs.xvg') # ’ダブり’があるので除去
     os.remove('rmsd_pacs_tmp.xvg')
 
 # 各ステップでのrmsdを記録
-def write_log(m):
-    o = open('log_pacs.txt','w')
-    for i in range(m):
-        rmsd = read_rmsd('rmsd_%d_%d.xvg'%(i,0))
-        o.write(str(rmsd[0]) + '\n')
+def write_log(rmsd):
+    o = open('log_pacs.txt','a')
+    o.write(str(rmsd) + '\n')
     o.close()
 
 
