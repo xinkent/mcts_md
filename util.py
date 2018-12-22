81266999fd35e327c8c974465fd0b2c1efedf095
xinkent <xinkent1994@gmail.com>
2018-12-22 13:27:50 +0900

    update

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 import pandas as pd
 # import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import pickle
 from pats_md import *
 # from pylab import *
